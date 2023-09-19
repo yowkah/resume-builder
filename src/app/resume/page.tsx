7df5fa6beae47f2cc6af4fc08af4f913711e3fde
Levi Schouten <levi.schouten.werk@gmail.com>
2023-09-19 15:05:20 +0200

    feat: introduced use-breakpoint hook

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -9,13 +9,8 @@ import ResumeForm, {
 } from "./_components/resume-form";
 import Resume from "./_components/resume";
 import { Button } from "@/components/ui/button";
-import {
-  ChevronLeftIcon,
-  DownloadIcon,
-  FileIcon,
-  Loader2Icon,
-} from "lucide-react";
-import { cn } from "@/lib/utils";
+import { ChevronLeftIcon, DownloadIcon, FileIcon } from "lucide-react";
+import { useBreakpoint } from "@/lib/utils";
 import PDFPreview from "@/components/shared/pdf-preview";
 
 import z from "zod";
@@ -79,6 +74,8 @@ export type Schema = z.infer<typeof schema>;
 export default function Builder() {
   const [showPreview, setShowPreview] = React.useState(false);
 
+  const isLargeScreen = useBreakpoint("lg");
+
   function calculateDefaultValues() {
     if (typeof window === "undefined") return defaultValues;
 
@@ -100,91 +97,67 @@ export default function Builder() {
 
   const formData = methods.watch();
 
-  const autoSave = React.useCallback(
+  const autoSave = React.useRef(
     debounce((data) => {
+      if (typeof window === "undefined") return;
+
       localStorage.setItem("resume", JSON.stringify(data));
-    }, 1000),
-    []
+    }, 1000)
   );
 
   React.useEffect(() => {
-    autoSave(formData);
+    autoSave.current(formData);
   }, [formData, autoSave]);
 
   return (
     <div className="flex flex-col lg:flex-row h-screen">
-      <div
-        className={cn(
-          "lg:flex-none flex-1 lg:basis-1/2 max-h-full overflow-auto",
-          {
-            "hidden lg:block": showPreview,
-          }
-        )}
-      >
-        <ResumeForm methods={methods} />
-        <Button
-          className="lg:hidden fixed right-8 bottom-8"
-          onClick={() => setShowPreview(true)}
-        >
-          <FileIcon className="w-4 h-4 mr-2" /> Preview
-        </Button>
-      </div>
-      <div
-        className={cn(
-          "fixed lg:relative h-full w-screen flex-none invisible lg:visible lg:basis-1/2 bg-muted",
-          {
-            "visible flex-col flex": showPreview,
-          }
-        )}
-      >
-        <div className="lg:hidden bg-primary text-white flex justify-between items-center">
-          <Button onClick={() => setShowPreview(false)}>
-            <ChevronLeftIcon className="w-4 h-4 mr-2" /> Back to editor
+      {(isLargeScreen || !showPreview) && (
+        <div className="lg:flex-none flex-1 lg:basis-1/2 max-h-full overflow-auto">
+          <ResumeForm methods={methods} />
+          <Button
+            className="lg:hidden fixed right-8 bottom-8"
+            onClick={() => setShowPreview(true)}
+          >
+            <FileIcon className="w-4 h-4 mr-2" /> Preview
           </Button>
-          <NoSSR>
-            <PDFDownloadLink
-              document={<Resume data={formData} />}
-              fileName="resume.pdf"
-            >
-              {({ blob, url, loading, error }) =>
-                loading ? (
-                  <Button size="lg" disabled>
-                    <Loader2Icon
-                      className={cn("mr-2 h-4 w-4 animate-spin", {
-                        hidden: !loading,
-                      })}
-                    />
-                    Download PDF
-                  </Button>
-                ) : (
-                  <Button size="lg">
-                    <DownloadIcon className="w-4 h-4 mr-2" /> Download
-                  </Button>
-                )
-              }
-            </PDFDownloadLink>
-          </NoSSR>
         </div>
-        <div className="overflow-hidden p-12 flex-1 space-y-4">
-          <div className="hidden lg:flex justify-end">
+      )}
+      {(isLargeScreen || showPreview) && (
+        <div className="lg:relative h-full w-screen flex-none lg:basis-1/2 bg-muted">
+          <div className="bg-primary text-white flex lg:hidden justify-between items-center">
+            <Button onClick={() => setShowPreview(false)}>
+              <ChevronLeftIcon className="w-4 h-4 mr-2" /> Back to editor
+            </Button>
             <NoSSR>
               <PDFDownloadLink
                 document={<Resume data={formData} />}
                 fileName="resume.pdf"
               >
-                {() => (
-                  <Button>
-                    <DownloadIcon className="w-4 h-4 mr-2" /> Download
-                  </Button>
-                )}
+                <Button size="lg">
+                  <DownloadIcon className="w-4 h-4 mr-2" /> Download
+                </Button>
               </PDFDownloadLink>
             </NoSSR>
           </div>
-          <PDFPreview>
-            <Resume data={formData} />
-          </PDFPreview>
+          <div className="overflow-hidden p-12 flex-1 space-y-4">
+            <div className="hidden lg:flex justify-end">
+              <NoSSR>
+                <PDFDownloadLink
+                  document={<Resume data={formData} />}
+                  fileName="resume.pdf"
+                >
+                  <Button>
+                    <DownloadIcon className="w-4 h-4 mr-2" /> Download
+                  </Button>
+                </PDFDownloadLink>
+              </NoSSR>
+            </div>
+            <PDFPreview>
+              <Resume data={formData} />
+            </PDFPreview>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
